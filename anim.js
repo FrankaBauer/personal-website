ab05630971b537a31baa20da17f4527dd5fe3401
FrankaBauer <franka.bauer@outlook.de>
2024-04-04 11:12:25 +0200

    first version mobile optimization

--- anim.js
+++ anim.js
@@ -9,3 +9,15 @@ const observer = new IntersectionObserver((entries)=> {
 
 const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
+
+function myFunction() {
+    var x = document.getElementById("dropdown");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+    
+    
+  }
+    
